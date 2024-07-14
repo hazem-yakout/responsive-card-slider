c0da3faa519c949fe6a45b2572cceec5f11161cf
Hazem yakout <143601439+hazem-yakout@users.noreply.github.com>
2024-07-14 07:52:32 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+const swiper = new Swiper(".slider-wrapper", {
+  // Optional parameters
+
+  loop: true,
+  grabCursor: true,
+  spaceBetween: 30,
+  // Pagination bullets
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breapoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
